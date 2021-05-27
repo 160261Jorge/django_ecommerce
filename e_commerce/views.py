b6e038684ba955cb5f3cb3785181c54ba050b4df
Jorge <sktjorge24@gmail.com>
2021-05-27 16:00:11 -0300

    url about_page, contact_page and def contact_page, about_page and upadate gitignore

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,4 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "pagina principal",
+        "content": "Bem-vindo a pagina principal"
+    }
+    return render(request, "home_page.html", context)
+
+
+def about_page(request):
+    context = {
+        "title": "pagina sobre",
+        "content": "Bem-vindo a pagina sobre"
+    }
+    return render(request, "about_page.html", context)
+
+
+def contact_page(request):
+    context = {
+        "title": "pagina de contato",
+        "content": "Bem-vindo a pagina de contato"
+    }
+    return render(request, "contact_page.html", context)
